7459de621e08943e9753b980bda8956d4deccff6
Sam7643 <159182339+Sam7643@users.noreply.github.com>
2024-04-02 11:05:37 -0700

    M5. L5.  Code Player Health + Practice

--- 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
+++ 05.loot_it_all_workbook/practices/L5.P1.code_an_energy_pack/ship_energy_pack.gd
@@ -10,7 +10,6 @@ var energy := 20.0
 func _ready() -> void:
 	get_node("UI/EnergyBar").value = energy
 
-
 func _process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
@@ -22,6 +21,10 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
 
-
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+	
 func _on_area_entered(area: Area2D) -> void:
-	pass
+	set_energy(energy + 20)
+
